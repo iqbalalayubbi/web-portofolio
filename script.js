f6efb059383834b9446b643142a689480a23ba12
iqbalalayubbi <iqbal.is415@gmail.com>
2024-02-28 13:25:01 +0700

    UPDATE: set up css style for light mode

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-$(document).ready(function () {
-    $(".image-popup-vertical-fit").magnificPopup({
-        type: "image",
-        mainClass: "mfp-with-zoom",
-        gallery: {
-            enabled: true,
-        },
+function lightMode() {
+    $("body").addClass("light-mode");
+}
 
-        zoom: {
-            enabled: true,
+function darkMode() {
+    $("body").removeClass("light-mode");
+}
 
-            duration: 300, // duration of the effect, in milliseconds
-            easing: "ease-in-out", // CSS transition easing function
-
-            opener: function (openerElement) {
-                return openerElement.is("img") ? openerElement : openerElement.find("img");
-            },
-        },
-    });
-});
+$("")
+    // lightMode();
+    .darkMode();
